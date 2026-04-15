89c6b3151c7c802ce5aa99a603100188181a9bbd
Ajay first project <ajaypalsinghr350@gmail.com>
2026-04-15 07:51:46 +0530

    changed nav bar color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-700 p-4 text-white shadow-lg flex justify-between items-center">
+    <nav className="bg-blue-950 p-4 text-white shadow-lg flex justify-between items-center">
       <h1 className="text-xl font-bold uppercase tracking-wider">My Billing Website</h1>
       <div className="space-x-4">
         <Link to="/admin" className="hover:bg-blue-700 px-3 py-2 rounded transition">Admin</Link>
